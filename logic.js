73567cae40073f87a7771e1b50f12aeb1be835ee
WYousefSaid <WYousefsaid@proton.me>
2022-09-24 19:10:09 +0200

    displaied 16x16 grid panel.

---
+++ logic.js
@@ -0,0 +1,15 @@
+let container = document.querySelector('.container');
+for (let i = 0; i<256; i++){
+    container.innerHTML += '<div class="box"></div>';
+}
+let hover = document.getElementsByClassName('box');
+for (let i = 0; i < hover.length; i++){
+    hover[i].addEventListener('mouseover', () =>{
+      mouseOver(i)
+    });
+}
+
+
+function mouseOver(i){
+    hover[i].classList.add('hover');
+}
