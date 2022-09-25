1c66f55c0806725f251dc74c5e02de812bd369ee
WYousefSaid <WYousefsaid@proton.me>
2022-09-25 17:14:53 +0200

    prompt on invalid input

--- logic.js
+++ logic.js
@@ -3,13 +3,18 @@ let btn = document.getElementById('gridBtn');
 btn.addEventListener('click',createGrid);
 
 function createGrid(){
-    let usrChoice = parseInt(prompt("Enter layout"));
+    let usrChoice = parseInt(prompt("Enter layout 1-100: "));
     container.innerHTML = "";
-    for (let i = 0; i<(usrChoice*usrChoice); i++){
-        const content = document.createElement('div');
-        content.classList.add('box')
-        container.appendChild(content);
+    if (usrChoice <= 100){
+        for (let i = 0; i<(usrChoice*usrChoice); i++){
+            const content = document.createElement('div');
+            content.classList.add('box')
+            container.appendChild(content);
+        }
+    }else{
+        let usrChoice = parseInt(prompt("Choose between 1-100 please: "));
     }
+
     //---hover start---
     let boxes = document.getElementsByClassName('box')
     for (let a = 0; a<boxes.length; a++){
